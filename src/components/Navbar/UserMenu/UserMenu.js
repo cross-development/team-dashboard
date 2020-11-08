bac860cb783d430f8c1adc7872a8d706a6cc5b52
Vitaliy Derda <derdavitaliy@gmail.com>
2020-11-08 21:53:57 +0200

    added dropdown menu

--- src/components/Navbar/UserMenu/UserMenu.js
+++ src/components/Navbar/UserMenu/UserMenu.js
@@ -1,16 +1,21 @@
 //Core
-import React from 'react';
+import React, { useState } from 'react';
 //Redux
 import { useDispatch, useSelector } from 'react-redux';
 import { signOutUser } from 'redux/auth/authOperations';
 //Styles
-import UserAvatar from 'assets/user.png';
+import UserAvatar from 'assets/user.jpg';
 import { FaChevronDown } from 'react-icons/fa';
+import { StyledMenu, StyledMenuItem, StyledMenuLink } from './UserMenu.styles';
 import { StylesContainer, StyledImg, StyledName, StyledDropdown } from './UserMenu.styles';
 
 const UserMenu = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	const { displayName } = useSelector(state => state.auth);
 	const dispatch = useDispatch();
+
+	const handleClick = () => setIsOpen(prevState => !prevState);
 
 	const handleSignOutUser = () => dispatch(signOutUser());
 
@@ -20,11 +25,39 @@ const UserMenu = () => {
 
 			<StyledName>{displayName}</StyledName>
 
-			<StyledDropdown onClick={handleSignOutUser}>
+			<StyledDropdown onClick={handleClick}>
 				<FaChevronDown />
+
+				{isOpen && (
+					<StyledMenu>
+						<StyledMenuItem>
+							<StyledMenuLink to="/profile">Profile</StyledMenuLink>
+						</StyledMenuItem>
+						<StyledMenuItem>
+							<StyledMenuLink to="/" onClick={handleSignOutUser}>
+								Sign Out
+							</StyledMenuLink>
+						</StyledMenuItem>
+					</StyledMenu>
+				)}
 			</StyledDropdown>
 		</StylesContainer>
 	);
 };
 
 export default UserMenu;
+
+// <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+//   Open Menu
+// </Button>
+// <Menu
+//   id="simple-menu"
+//   anchorEl={anchorEl}
+//   keepMounted
+//   open={Boolean(anchorEl)}
+//   onClose={handleClose}
+// >
+//   <MenuItem onClick={handleClose}>Profile</MenuItem>
+//   <MenuItem onClick={handleClose}>My account</MenuItem>
+//   <MenuItem onClick={handleClose}>Logout</MenuItem>
+// </Menu>
--- src/components/Navbar/UserMenu/UserMenu.styles.js
+++ src/components/Navbar/UserMenu/UserMenu.styles.js
@@ -1,5 +1,6 @@
 //Core
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const StylesContainer = styled.div`
 	display: flex;
@@ -24,6 +25,7 @@ export const StyledName = styled.p`
 `;
 
 export const StyledDropdown = styled.div`
+	position: relative;
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -32,7 +34,43 @@ export const StyledDropdown = styled.div`
 	transition: all 250ms linear;
 
 	&:hover {
-		transform: scale(1.2);
 		color: #507bfc;
+	}
+`;
+
+export const StyledMenu = styled.ul`
+	position: absolute;
+	top: 20px;
+	left: -90px;
+	width: auto;
+	height: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: flex-start;
+`;
+
+export const StyledMenuItem = styled.li`
+	background-color: #ffffff;
+	width: 100%;
+	list-style: none;
+
+	&:hover {
+		background-color: #507bfc;
+		box-shadow: 0px 10px 20px 0px rgba(80, 123, 252, 0.8);
+	}
+`;
+
+export const StyledMenuLink = styled(Link)`
+	color: #98a0a6;
+	padding: 0.8rem 1rem;
+	font-size: 16px;
+	text-decoration: none;
+	display: block;
+	height: 100%;
+	width: 100%;
+
+	&:hover {
+		color: #ffffff;
 	}
 `;
